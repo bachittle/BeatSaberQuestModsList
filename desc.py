b0b15060f74e6ff4926efe0a2f2c8d3f11859011
bachittle <bachittle@gmail.com>
2020-04-15 15:02:44 -0400

    fixed spacing issue on github

--- desc.py
+++ desc.py
@@ -8,9 +8,9 @@ for mod_name in table_json.keys():
     item = table_json[mod_name]
     print("Please enter a description for " + mod_name + " (type in !quit to escape)")
     desc = ""
-    while desc[-7:] != '!quit\n': 
-        desc += input() + "\n"
-    desc = desc[:-6]
+    while desc[-7:] != '!quit\n\n': 
+        desc += input() + "\n\n"
+    desc = desc[:-7]
     if desc: item["desc"] = desc
     image = input("Enter a link to an image, or just press enter to skip: ")
     if image: item["image"] = image
